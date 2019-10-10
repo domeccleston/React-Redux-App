e4bfe121027b1d26921d32405167319fc688ed54
Dom <domeccleston@gmail.com>
2019-10-10 18:52:22 +0100

    formatted with prettier

--- react-redux-app/src/components/Counter.jsx
+++ react-redux-app/src/components/Counter.jsx
@@ -1,17 +1,20 @@
-import * as actionCreators from '../state/actionCreators';
-import { connect } from 'react-redux';
-import React from 'react';
+import * as actionCreators from "../state/actionCreators";
+import { connect } from "react-redux";
+import React from "react";
 
 export function Counter(props) {
-    const { count, increment, decrement, reset } = props;
-    return (
-        <div>
-            The count is {count}
-            <button onClick={increment}>+</button>
-            <button onClick={decrement}>-</button>
-            <button onClick={reset}>Reset</button>
-        </div>
-    )
+  const { count, increment, decrement, reset } = props;
+  return (
+    <div>
+      The count is {count}
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
 }
 
-export default connect(state => state, actionCreators)(Counter)
+export default connect(
+  state => state,
+  actionCreators
+)(Counter);
--- react-redux-app/src/index.js
+++ react-redux-app/src/index.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import Counter from './components/Counter';
-import Market from './components/Market';
-import Search from './components/Search';
-import Quote from './components/Quote';
-import thunk from 'redux-thunk';
-import * as reducers from './state/reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import Counter from "./components/Counter";
+import Market from "./components/Market";
+import Search from "./components/Search";
+import Quote from "./components/Quote";
+import thunk from "redux-thunk";
+import * as reducers from "./state/reducers";
 
 const rootReducer = combineReducers({
   count: reducers.countReducer,
   cart: reducers.cartReducer,
   stock: reducers.stockReducer,
-  quote: reducers.quoteReducer,
-})
+  quote: reducers.quoteReducer
+});
 
 const store = createStore(
   rootReducer,
   {},
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
-)
+);
 
 ReactDOM.render(
   <div class="App">
-  <Provider store={store}>
-{/*     <Counter />
+    <Provider store={store}>
+      {/*     <Counter />
     <Market /> */}
-    <Quote />
-    <Search />
-  </Provider>
-  </div>, document.getElementById('root'));
+      <Quote />
+      <Search />
+    </Provider>
+  </div>,
+  document.getElementById("root")
+);
